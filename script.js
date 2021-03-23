2df97613748b0f2f8ed5023a2698602dcbba0c6a
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-23 19:38:49 +0100

    3.3 Lexical Environment

---
+++ script.js
@@ -0,0 +1,17 @@
+///////////////////////
+// 3.3 Lexical Environment
+
+function printName() {
+	return 'Andrei Neagoie'
+}
+
+function findName() {
+	function a() {}
+	return printName()
+}
+
+function sayMyName() {
+	return findName()
+}
+
+window.sayMyName()
