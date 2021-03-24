db99d02edb9cc196fa90139607ea31d4b7488c02
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-24 09:46:31 +0100

    3.9 Arguments Keyword

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ var canada = function () {
 	console.log('cold')
 }
 */
-
+/*
 var canada = () => {
 	console.log('cold')
 }
@@ -119,10 +119,36 @@ function india() {
 // Function Invocation/Call/Execution
 canada()
 india()
-
+*/
+/*
 function marry(person1, person2) {
 	console.log('arguments', arguments)
+	console.log(Array.from(arguments))
 	return `${person1} is now married to ${person2}`
 }
 
 console.log(marry('Tim', 'Tina'))
+*/
+/*
+function marry2(...args) {
+	console.log('arguments', args)
+	console.log(Array.from(arguments))
+	return `${args[0]} is now married to ${args[1]}`
+}
+
+console.log(marry2('Tim', 'Tina'))
+*/
+
+var canada = () => {
+	console.log('cold')
+}
+
+// Function Declaration
+function india() {
+	console.log(arguments)
+	console.log('warm')
+}
+
+// Function Invocation/Call/Execution
+// canada()
+india()
