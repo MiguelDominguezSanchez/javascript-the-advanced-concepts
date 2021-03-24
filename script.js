25006776dc8f1fb4428bc71ec65aa293002501c8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-24 08:50:22 +0100

    3.4 Hoisting

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ///////////////////////
 // 3.3 Lexical Environment
 
+/*
 function printName() {
 	return 'Andrei Neagoie'
 }
@@ -15,3 +16,22 @@ function sayMyName() {
 }
 
 window.sayMyName()
+*/
+
+///////////////////////
+// 3.4 Hoisting
+
+console.log('1--------')
+console.log(teddy)
+console.log(sing2)
+var teddy = 'bear'
+
+// function expression
+var sing2 = function () {
+	console.log('uhhh la la la')
+}
+
+// function declaration
+function sing() {
+	console.log('ohhh la la la')
+}
