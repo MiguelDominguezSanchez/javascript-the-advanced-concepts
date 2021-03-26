4b3678860de0ccb57414dbf225d758ea2c8fbeee
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-26 16:46:18 +0100

    4.4 Pass By Reference vs Pass By Value

--- script.js
+++ script.js
@@ -685,5 +685,67 @@ console.log(typeof [])
 var something
 */
 
+/*
 console.log(Array.isArray([1, 2, 3]))
 console.log(Array.isArray({}))
+*/
+/*
+var a = 5
+var b = 10
+*/
+
+var a = 5
+var b = a
+
+b++
+
+/*
+console.log(a)
+console.log(b)
+*/
+
+/*
+let obj1 = { name: 'Yao', password: '123' }
+let obj2 = obj1
+
+obj2.password = 'easypeasy'
+
+console.log(obj1)
+console.log(obj2)
+*/
+
+/*
+var c = [1, 2, 3, 4, 5]
+var d = [].concat(c)
+d.push(187628761)
+console.log(c)
+console.log(d)
+*/
+
+/*
+let obj = { a: 'a', b: 'b', c: 'c' }
+let clone = Object.assign({}, obj)
+let clone2 = { ...obj }
+
+obj.c = 5
+console.log(obj)
+console.log(clone)
+console.log(clone2)
+*/
+
+let obj = {
+	a: 'a',
+	b: 'b',
+	c: {
+		deep: 'try and copy me',
+	},
+}
+let clone = Object.assign({}, obj)
+let clone2 = { ...obj }
+let superClone = JSON.parse(JSON.stringify(obj))
+
+obj.c.deep = 'hahaha'
+console.log(obj)
+console.log(clone)
+console.log(clone2)
+console.log(superClone)
