c7fad2e55d162fd616e8c5a611172078a2c9b4b2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-28 11:48:52 +0200

    5.14 Solution Closures 3

--- script.js
+++ script.js
@@ -1224,9 +1224,32 @@ console.log(view)
 ///////////////////////
 // 5.13 Exercise Closures 3
 
+/*
 const array = [1, 2, 3, 4]
 for (var i = 0; i < array.length; i++) {
 	setTimeout(function () {
 		console.log('I am at index ' + i)
 	}, 3000)
+}
+*/
+
+///////////////////////
+// 5.13 Solution Closures 3
+
+/*
+const array = [1, 2, 3, 4]
+for (let i = 0; i < array.length; i++) {
+		setTimeout(function () {
+			console.log('I am at index ' + array[i])
+		}, 3000)
+}
+*/
+
+const array = [1, 2, 3, 4]
+for (let i = 0; i < array.length; i++) {
+	;(function (closureI) {
+		setTimeout(function () {
+			console.log('I am at index ' + array[closureI])
+		}, 3000)
+	})(i)
 }
