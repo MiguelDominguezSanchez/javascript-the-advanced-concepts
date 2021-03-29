520099cef09d63f351f89095ca4c592546a75150
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-29 17:56:49 +0200

    6.6 OOP3 Constructor Functions

--- script.js
+++ script.js
@@ -1485,6 +1485,7 @@ sam.attack = elfFunctions.attack
 console.log(sam.attack())
 */
 
+/*
 // Object.create()
 const elfFunctions = {
 	attack() {
@@ -1502,3 +1503,42 @@ const peter = createElf('Peter', 'stones')
 console.log(peter.attack())
 const sam = createElf('Sam', 'fire')
 console.log(sam.attack())
+*/
+
+///////////////////////
+// 6.6 OOP3 Constructor functions
+
+// Constructor Functions
+function Elf(name, weapon) {
+	this.name = name
+	this.weapon = weapon
+}
+
+// Elf.prototype.attack = function () {
+// 	return 'attack with ' + this.weapon
+// }
+
+Elf.prototype.attack = function () {
+	return 'attack with ' + this.weapon
+}
+
+// Elf.prototype.build = function () {
+// 	return 'attack with ' + this.weapon
+// }
+
+const peter = new Elf('Peter', 'stones')
+console.log(peter.attack())
+const sam = new Elf('Sam', 'fire')
+// console.log(sam.name)
+console.log(sam.attack())
+// console.log(sam.attack())
+
+// const Elf1 = new Function(
+// 	'name',
+// 	'weapon',
+// 	`this.name = name
+// 	this.name = weapon`
+// )
+
+// const sarah = new Elf1('Sarah', 'fireworks')
+// console.log(sarah)
