97eab3a49e0f1060515cbf3d65e7dfb781b108c7
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-30 11:44:27 +0200

    7.3 Exercise Amazon

--- script.js
+++ script.js
@@ -1742,3 +1742,24 @@ console.log(dolby instanceof Character)
 
 ///////////////////////
 // 6.14 Public vs Private
+
+///////////////////////
+// 7.3 Exercise Amazon
+
+// Amazon shopping
+const user = {
+	name: 'Kim',
+	active: true,
+	cart: [],
+	purchases: [],
+}
+
+// Implement a cart feature:
+// 1. Add items to cart.
+// 2. Add 3% tax to item in cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
+
+// Bonus:
+// accept refunds.
+// Track user history
