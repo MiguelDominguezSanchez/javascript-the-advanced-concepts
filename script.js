9a9b07dd1e3f7e4150d7a6b8142953d35d5c7810
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-31 11:18:42 +0200

    7.11 Currying

--- script.js
+++ script.js
@@ -1880,7 +1880,7 @@ console.log(obj, updatedObj)
 
 ///////////////////////
 // 7.10 Higher Order Function and Closures
-
+/*
 // HOF
 const hof = fn => fn(5)
 console.log(
@@ -1901,3 +1901,20 @@ const getCounter = closure()
 console.log(getCounter())
 console.log(getCounter())
 console.log(getCounter())
+*/
+
+///////////////////////
+// 7.11 Currying
+
+// currying
+const multiply = (a, b) => a * b
+// console.log(multiply(3, 4))
+const curriedMultiply = a => b => a * b
+// console.log(curriedMultiply(5, 3))
+// console.log(curriedMultiply(5)(3))
+const curriedMultiplyBy5 = curriedMultiply(5)
+
+// 10 years
+console.log(curriedMultiplyBy5(4))
+console.log(curriedMultiplyBy5(4))
+console.log(curriedMultiplyBy5(4))
