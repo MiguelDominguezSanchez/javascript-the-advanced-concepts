1724ebab44ad87812ea86a9697b4592b44ee9b23
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-31 11:52:00 +0200

    15. MCI Memoization 2

--- script.js
+++ script.js
@@ -1931,8 +1931,8 @@ console.log(curriedMultiplyBy5(4, 10))
 */
 
 ///////////////////////
-// 7.14 MCI Memoization
-
+// 7.14 MCI Memoization 1
+/*
 function addTo80(n) {
 	console.log('long time')
 	return n + 80
@@ -1957,3 +1957,28 @@ function memoizedAddTo80(n) {
 
 console.log('1', memoizedAddTo80(5))
 console.log('2', memoizedAddTo80(6))
+*/
+
+///////////////////////
+// 7.14 MCI Memoization 2
+
+function addTo80(n) {
+	console.log('long time')
+	return n + 80
+}
+
+function memoizedAddTo80() {
+	let cache = {}
+	return function (n) {}
+	if (n in cache) {
+		return cache[n]
+	} else {
+		console.log('long time')
+		cache[n] = n + 80
+		return cache[n]
+	}
+}
+const memoized = memoizedAddTo80()
+
+console.log('1', memoized(5))
+console.log('2', memoized(5))
