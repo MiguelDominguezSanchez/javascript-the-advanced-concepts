14246ccca8447235dd40cca54a5165b17cce3bbd
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-05 12:14:02 +0200

    9.4 Promises

--- script.js
+++ script.js
@@ -2118,8 +2118,8 @@ amazonHistory
 // function goForward() {}
 
 ///////////////////////////////
-
 // Amazon shopping
+/*
 const user = {
 	name: 'Kim',
 	active: true,
@@ -2168,7 +2168,7 @@ function emptyCart(user) {
 	amazonHistory.push(user)
 	return Object.assign({}, user, { cart: [] })
 }
-
+*/
 // Implement a cart feature:
 // 1. Add items to cart.
 // 2. Add 3% tax to item in cart
@@ -2186,7 +2186,7 @@ function emptyCart(user) {
 
 // function refundItem() {}
 
-amazonHistory
+// amazonHistory
 
 /*
 [
@@ -2204,3 +2204,85 @@ amazonHistory
 
 // function getUserState() {}
 // function goForward() {}
+
+///////////////////////////////
+// 7.20 Reviewing FP
+
+///////////////////////////////
+// 8.1 Composition vs Inheritance
+
+///////////////////////////////
+// 8.2 OOP vs FP
+
+///////////////////////////////
+// 8.3 OOP vs FP 2
+
+///////////////////////////////
+// 9.4 Promises
+
+const promise = new Promise((resolve, reject) => {
+	if (true) {
+		resolve('stuff worked')
+	} else {
+		reject('Error, it broke')
+	}
+})
+
+// promise.then(result => console.log(result))
+
+// Chaining Promises
+/*
+promise
+	.then(result => {
+		throw Error
+		return result + '!'
+	})
+	.then(result2 => {
+		console.log(result2)
+	})
+	.catch(() => console.log('errroor!'))
+*/
+
+/*
+const promise2 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 100, 'HIII')
+})
+
+const promise3 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 1000, 'POOKIE')
+})
+
+const promise4 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 3000, 'Is it me you are looking for?')
+})
+
+Promise.all([promise, promise2, promise3, promise4]).then(values => {
+	console.log(values)
+})
+
+promise
+	.then(result => result + '!')
+	.then(result2 => result2 + '?')
+	.catch(() => console.log('errroor!'))
+	.then(result3 => {
+		console.log(result3 + '!')
+	})
+*/
+
+const urls = [
+	'https://jsonplaceholder.typicode.com/users',
+	'https://jsonplaceholder.typicode.com/posts',
+	'https://jsonplaceholder.typicode.com/albums',
+]
+
+Promise.all(
+	urls.map(url => {
+		return fetch(url).then(resp => resp.json())
+	})
+)
+	.then(results => {
+		console.log(results[0])
+		console.log(results[1])
+		console.log(results[2])
+	})
+	.catch(() => console.log('error'))
