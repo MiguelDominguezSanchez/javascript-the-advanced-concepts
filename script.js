ec482fb7a687e0321f82b660aab69e960e0d1d0e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-07 11:47:05 +0200

    9.7 ES6 (ES2018)

--- script.js
+++ script.js
@@ -2349,7 +2349,7 @@ const getData1 = async function () {
 
 ///////////////////////////////
 // 9.6 ES9 (ES2018)
-
+/*
 // Object Spread Operator
 const animals = {
 	tiger: 23,
@@ -2357,14 +2357,14 @@ const animals = {
 	monkey: 2,
 	bird: 40,
 }
-
+*/
 /*
 const array = [1, 2, 3, 4, 5]
 function sum(a, b, c, d, e) {
 	return a + b + c + d + e
 }
 */
-
+/*
 function objectSpread(p1, p2, p3) {
 	console.log(p1)
 	console.log(p2)
@@ -2374,3 +2374,67 @@ function objectSpread(p1, p2, p3) {
 const { tiger, lion, ...rest } = animals
 
 objectSpread(tiger, lion, rest)
+*/
+///////////////////////////////
+// 9.7 ES6 (ES2018)
+// finally
+/*
+const urls = [
+	'https://swapi.co/api/people/1',
+	'https://swapi.co/api/people/2',
+	'https://swapi.co/api/people/3',
+	'https://swapi.co/api/people/4',
+]
+
+Promise.all(
+	urls.map(url => {
+		return fetch(url).then(people => people.json())
+	})
+)
+	.then(array => {
+		throw Error
+		console.log('1', array[0])
+		console.log('2', array[1])
+		console.log('3', array[2])
+		console.log('4', array[3])
+	})
+	.catch(err => console.log('ughhhh fix it!', err))
+	.finally(() => console.log('extra'))
+*/
+
+// for await of
+const urls = [
+	'https://jsonplaceholder.typicode.com/users',
+	'https://jsonplaceholder.typicode.com/postss',
+	'https://jsonplaceholder.typicode.com/albums',
+]
+
+const getData = async function () {
+	try {
+		const [users, posts, albums] = await Promise.all(
+			urls.map(url => fetch(url).then(resp => resp.json())),
+			console.log('users', users),
+			console.log('posts', posts),
+			console.log('albums', albums)
+		)
+	} catch (err) {
+		console.log('oops', err)
+	}
+}
+
+/*
+const loopThroughUrls = url => {
+	for (url of urls) {
+		console.log(url)
+	}
+}
+console.log(loopThroughUrls(urls))
+*/
+
+const getData2 = async function () {
+	const arrayOfPromises = urls.map(url => fetch(url))
+	for await (let request of arrayOfPromises) {
+		const data = await request.json()
+		console.log(data)
+	}
+}
