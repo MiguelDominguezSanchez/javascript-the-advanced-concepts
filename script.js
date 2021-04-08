127275ec54fba5c0309024af0ee5c2e8a173b5fb
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-08 12:48:01 +0200

    10.5 CommonJS, AMD, UMD

--- script.js
+++ script.js
@@ -2562,3 +2562,29 @@ var fightModule = (function () {
 
 ///////////////////////////////
 // 10.4 Module Pattern Pros and Cons
+
+///////////////////////////////
+// 10.5 Common JS, AMD, UMD
+// CommonJS and AMD Asynchronous Module Definition
+
+var module1 = require('module1') //.fight;
+var module2 = require('module2') //.importedFunc2
+
+function fight() {}
+
+module1.exports = {
+	fight: fight,
+}
+
+// AMD
+
+define(['module1', 'module2'], function (module1Import, module2Import) {
+	var module1 = module1Import //.fight;
+	var module2 = module2Import //.importedFunc2
+
+	function dance() {}
+
+	return {
+		dance: dance,
+	}
+})
