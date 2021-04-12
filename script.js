412cace1a0d06c860ab37767436c9e87acf7517c
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-12 13:27:57 +0200

    15.3 Advanced Control Flow

--- script.js
+++ script.js
@@ -3551,8 +3551,88 @@ console.log(fun)
 */
 
 // var fun = 5
-
+/*
 function funestFunction() {
 	// child scope
 	console.log(fun)
 }
+*/
+
+///////////////////////
+// 15.3 Advanced Control Flow
+
+// condition ? expr1 : expr2
+
+/*
+function isUserValid(bool) {
+	return bool
+}
+
+var answer = isUserValid(false) ? 'You may enter' : 'Access Denied'
+
+console.log(answer)
+*/
+/*
+function isUserValid(bool) {
+	return bool
+}
+
+var answer = isUserValid(true) ? 'You may enter' : 'Access Denied'
+
+// console.log(answer)
+
+var automatedAnswer =
+	'Your account # is ' + isUserValid(false ? '12334' : 'not available')
+
+console.log(automatedAnswer)
+*/
+
+/*
+function isUserValid(bool) {
+	return bool
+}
+
+var answer = isUserValid(true) ? 'You may enter' : 'Access Denied'
+
+// var automatedAnswer =
+// 	'Your account # is ' + isUserValid(true ? '12334' : 'not available')
+
+// console.log(automatedAnswer)
+
+function condition() {
+	if (isUserValid(true)) {
+		return 'You may enter'
+	} else {
+		return 'Access denied'
+	}
+}
+
+var answer2 = condition()
+*/
+
+function moveCommand(direction) {
+	var whatHappens
+	switch (direction) {
+		case 'forward':
+			whatHappens = 'you encounter a monster'
+			break
+		case 'back':
+			whatHappens = 'you arrived home'
+			break
+		case 'right':
+			whatHappens = 'you found a river'
+			break
+		case 'left':
+			whatHappens = 'you run into a troll'
+			break
+		default:
+			whatHappens = 'please enter a valid direction'
+	}
+	return whatHappens
+}
+
+console.log(moveCommand('forward'))
+console.log(moveCommand('back'))
+console.log(moveCommand('right'))
+console.log(moveCommand('left'))
+console.log(moveCommand('asDFGD'))
