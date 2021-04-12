94a73abab5a38e9d125b834b4ba591145680f613
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-12 11:32:55 +0200

    14.14 Javascript Keywords

--- script.js
+++ script.js
@@ -3408,7 +3408,7 @@ do {
 
 ///////////////////////
 // 14.13 Exercise Build Facebook 2
-
+/*
 var database = [
 	{
 		username: 'andrei',
@@ -3463,3 +3463,44 @@ var userNamePrompt = prompt("what's your username?")
 var passwordPrompt = prompt("What's your password")
 
 sigIn(userNamePrompt, passwordPrompt)
+*/
+
+///////////////////////
+// 14.14 Javascript keyword
+
+// JAVASCRIPT KEYWORDS
+/*
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+export
+extends
+finally
+for
+function
+if
+import
+in
+instanceof
+new
+return
+super
+switch
+this
+throw
+try
+typeof
+var
+void
+while
+with
+yield
+*/
