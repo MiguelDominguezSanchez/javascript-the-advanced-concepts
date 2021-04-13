934aaa716eb7d218fb1c0fba25d5f1b5dccb0a36
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-13 12:20:40 +0200

    15.5 Advanced Functions

--- script.js
+++ script.js
@@ -3790,11 +3790,80 @@ function greet(name = '', age = 30, pet = 'cat') {
 // 	return a + b
 // }
 
-function add(a, b) {
-	return a + b
-}
+// function add(a, b) {
+// 	return a + b
+// }
 
-const add2 = (a, b) => a + b
+// const add2 = (a, b) => a + b
 
 ///////////////////////
 // 15.5 Advanced Functions
+/*
+function first() {
+	var greet = 'Hi'
+	function second() {
+		alert(greet)
+	}
+	return second
+}
+
+const newFunc = first()
+newFunc()
+*/
+/*
+const first = () => {
+	const greet = 'Hi'
+	const second = () => {
+		const name = 'bobby'
+		alert(greet)
+	}
+	return second
+}
+
+const newFunc = first()
+newFunc()
+*/
+// Closures - a function ran, the function executed. it´s never going to execute again.
+// But it´s going to remember that there are references to those variables
+// so the child scope always has access to the parent scope.
+
+// Currying
+/*
+const multiply = (a, b) => a * b
+const curriedMultiply = a => b => a * b
+curriedMultiply(3)
+*/
+
+/*
+const multiply = (a, b) => a * b
+const curriedMultiply = a => b => a * b
+curriedMultiply(3)
+*/
+
+/*
+const multiply = (a, b) => a * b
+const curriedMultiply = a => b => a * b
+const multiplyBy5 = curriedMultiply(5)
+*/
+
+// console.log(curriedMultiply(3)(4))
+
+/*
+console.log(multiplyBy5(5))
+console.log(multiplyBy5(10))
+console.log(multiplyBy5(11))
+*/
+
+// Compose
+const compose = (f, g) => a => f(g(a))
+
+const sum = num => num + 1
+
+console.log(compose(sum, sum)(5))
+
+// Avoiding Side Effects, functional purity
+
+var a = 1
+function b() {
+	a = 2
+}
