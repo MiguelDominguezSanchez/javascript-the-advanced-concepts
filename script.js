28ba09070b12e8d3494ad8f3b0b22540a942b79e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-13 18:31:52 +0200

    15.6 Advanced Arrays

--- script.js
+++ script.js
@@ -3855,15 +3855,63 @@ console.log(multiplyBy5(11))
 */
 
 // Compose
+/*
 const compose = (f, g) => a => f(g(a))
 
 const sum = num => num + 1
 
 console.log(compose(sum, sum)(5))
+*/
 
 // Avoiding Side Effects, functional purity
 
+/*
 var a = 1
 function b() {
 	a = 2
 }
+*/
+
+///////////////////////
+// 15.6 Advanced Arrrays
+
+const array = [1, 2, 10, 16]
+
+const double = []
+const newArray = array.forEach(num => {
+	double.push(num * 2)
+})
+
+console.log('forEach', double)
+
+// map, filter, reduce
+
+// const mapArray = array.map(num => {
+// 	return num * 2
+// })
+
+const mapArray = array.map(num => num * 2)
+
+console.log('map', mapArray)
+
+// filter
+
+// const filterArray = array.filter(num => {
+// 	return num > 5
+// })
+
+const filterArray = array.filter(num => num === 5)
+
+console.log('filter', filterArray)
+
+// reduce
+/*
+const reduceArray = array.reduce((accumulator, num) => {
+	return accumulator + num
+}, 0)
+*/
+const reduceArray = array.reduce((accumulator, num) => {
+	return accumulator + num
+}, 5)
+
+console.log('reduce', reduceArray)
